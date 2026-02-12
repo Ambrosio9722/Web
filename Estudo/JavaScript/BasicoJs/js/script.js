130664cd482270bba12310cb5df6bf0394b5f1e1
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-12 15:03:08 -0300

    null e undefined
    conteúdo null e undefined para arquivo externo

---
+++ Estudo/JavaScript/BasicoJs/js/script.js
@@ -0,0 +1,26 @@
+    //STRING
+var nome = "Antonio"
+
+// NUMBER
+var numero = 15
+var numero2 = 12.34
+
+// BOLEAN
+var ativado = true;
+
+alert(numero2) //abrir um dialog
+document.write(numero2)// escrever na tela
+console.log(numero2)
+
+//--------CONCATENAÇÃO------------
+
+var nomeTitulo = "Maria"
+var idadeTitulo = prompt("qual a sua idade: ")
+document.write("<h1>Ola " +nomeTitulo+ " como vai, idade:  </h1>" +idadeTitulo)
+
+//--------Null e Underfined ------------
+// Null = ausencia intencial de valor
+var teste = null
+// undefined = a variavel nã opossiu nenhum valor
+var teste2
+var teste3 = undefined

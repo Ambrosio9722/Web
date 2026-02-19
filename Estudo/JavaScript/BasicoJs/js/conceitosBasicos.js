6d81b1844de50ad84e08fb0684903bfcaf0b1698
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-19 11:11:00 -0300

    Casting de tipos
    conversão de tipos

---
+++ Estudo/JavaScript/BasicoJs/js/conceitosBasicos.js
@@ -0,0 +1,10 @@
+// CONVERTER VALORES //
+parseInt( ) // converte a variavael para inteiro
+parseFloat()// converte a variavel para o tipo float
+.toString() // converte uma variavel para o tipo string
+
+// OPERADORES LÓGICOS // 
+
+//E = %%
+//OU = ||
+//NEGAÇÃO = !

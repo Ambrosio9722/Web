1b3b903c8ecc15de832d6cfd5ca6d11beaa1713c
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-19 10:53:57 -0300

    exercício de soma de notas
    soma 3 notas no prompt e utiliza if/else

---
+++ Estudo/JavaScript/BasicoJs/js/aprovado.js
@@ -0,0 +1,19 @@
+alert("Media!")
+
+var nota1= Number(prompt("Nota1: "))
+var nota2= Number(prompt("Nota2: "))
+var nota3= Number(prompt("Nota3: "))
+
+var media = 60
+
+var notaFinal = nota1 + nota2 + nota3 
+document.write(notaFinal)
+if(notaFinal >= media){
+ document.write(" Passou!")
+}
+else if (notaFinal < media && notaFinal >= 40){
+  document.write("Recuperação!")
+}
+else{
+    document.write("Reprovou!")
+}

b1a6302709e264bce42a6295786a188377f287a5
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-21 16:32:44 -0300

    DOM- Selecionando elementos

---
+++ Estudo/JavaScript/BasicoJs/js/dom.js
@@ -0,0 +1,10 @@
+// selecionar um elemento atraves do seu ID 
+document.getElementById("nome_usuario")
+// seleciona um elemento atraves do seu nome da teg
+document.getElementsByTagName("input")
+// seleciona um elemento atraves do nome da classe
+document.getElementsByClassName("campo_texto")
+// seleciona um elemento atraves do nome do atributo
+document.getElementsByName("nome")
+// EXEMPLO
+console.log(document.getElementById("nome_usuario"))

1f73cf0f213afec9394a46097a5a62f8d308b5fe
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-24 18:09:12 -0300

    timing

--- Estudo/JavaScript/BasicoJs/js/BOM.js
+++ Estudo/JavaScript/BasicoJs/js/BOM.js
@@ -18,3 +18,10 @@ window.location.href = "https://www.google.com" //coloca u url no lugar da atual
 
 
 // Timing // 
+//(executar a ação apos um tempo) \\ milessegundos
+
+//setTimeout  = faz a ação apos um tempo 
+setTimeout(function(){}, 2000)//2 segundos
+
+//setInterval = faz a ação a cada contagem de tempoS
+setInterval((function(){}, 2000))

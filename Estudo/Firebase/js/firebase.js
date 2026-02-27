af9913fbec40de0b1128d7da529570e52376a8f2
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-27 18:50:36 -0300

    Criação do projeto no firebase
    add do gitignore dos dados sensíveis

---
+++ Estudo/Firebase/js/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCK-sF5skbqIaNfvFIWH9DypPshYkLbmTk",
+    authDomain: "todolist-495bf.firebaseapp.com",
+    projectId: "todolist-495bf",
+    storageBucket: "todolist-495bf.firebasestorage.app",
+    messagingSenderId: "553100544555",
+    appId: "1:553100544555:web:9e5c8628c9138bb4fbf86e"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);

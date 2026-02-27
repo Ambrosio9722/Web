c23ba5ea423663df0b408b6a265fcb0ef3505dac
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-02-27 18:30:48 -0300

    Javascript da pagina de login
    eventos nos botões e trocas de paginas de login e cadastro

---
+++ Estudo/Firebase/js/utils.js
@@ -0,0 +1,34 @@
+//definindo referencias para elementos da página
+var authForm = document.getElementById('authForm')
+var authFormTitle = document.getElementById('authFormTitle')
+var register = document.getElementById('register')
+var access= document.getElementById('access')
+
+// Alterar o formulario de autenticação para o cadastro de novas contas
+
+function toggleToRegister(){
+    authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
+    authFormTitle.innerHTML = 'Acesse a sua conta para cadastrar'
+    hideItem(register)
+    showitem(access)
+}
+
+// Alterar o formulario de autenticação para o acesso de contas ja existentes 
+
+function toggleToAccess(){
+    authForm.submitAuthForm.innerHTML = 'Acessar'
+    authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
+    hideItem(access)
+    showitem(register)
+}
+
+// Simplifica a exibição de elementos da página
+
+function showitem(element){
+  element.style.display = 'block'
+}
+// Simplifica a remoção de elementos da página
+
+function hideItem(element){
+  element.style.display = 'none'
+}

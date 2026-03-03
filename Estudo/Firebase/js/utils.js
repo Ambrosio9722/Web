dc589b259cad5a1c98cdd884e8ca9f900e55e6ff
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-03-03 17:14:23 -0300

    Hospedando site

--- Estudo/Firebase/js/utils.js
+++ Estudo/Firebase/js/utils.js
@@ -102,5 +102,5 @@ function showError(prefix, error){
 
 //atributos extras de configuração de e-mail
 var actionCodeSettings ={
-  url : 'http://127.0.0.1:5500/'
+  url : 'https://todolist-495bf.firebaseapp.com'
 }

142afa748b45b81784d122e033ab4279cb457156
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2026-03-04 17:03:25 -0300

    Inserindo dados no database

--- Estudo/Firebase/js/firebase.js
+++ Estudo/Firebase/js/firebase.js
@@ -5,8 +5,10 @@
     projectId: "todolist-495bf",
     storageBucket: "todolist-495bf.firebasestorage.app",
     messagingSenderId: "553100544555",
-    appId: "1:553100544555:web:9e5c8628c9138bb4fbf86e"
+    appId: "1:553100544555:web:9e5c8628c9138bb4fbf86e",
+    databaseURL: "https://todolist-495bf-default-rtdb.firebaseio.com/",
   };
 
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
--- Estudo/Firebase/js/todo.js
+++ Estudo/Firebase/js/todo.js
@@ -1,14 +1,19 @@
-// trata a submissão do formulario de tarefas
-todoForm.onsubmit = function (event){
-    event.preventDefaut() // evita o redirecionamento da pagina
-    if(todoForm.name.value != ''){
-      var data = {
-        name: todoForm.name.value
-      }
-      dbRefUsers.child(firebase.auth().currentUser.uid).push(data).then(function (){
-        console.log('Tarefa '+data.name+'adicionada com sucesso')
-      })
-    }else{
-        alert('O nome da tarefa não pode ser em branco para criar a tarefa')
+// Trata a submissão do formulário de autenticação
+todoForm.onsubmit = function (event) {
+  event.preventDefault() // Evita o redirecionamento da página
+  if (todoForm.name.value != '') {
+    var data = {
+      name: todoForm.name.value
     }
+
+    dbRefUsers.child(firebase.auth().currentUser.uid).push(data).then(function () {
+      console.log('Tarefa "' + data.name + '" adicionada com sucesso')
+    })
+  } else {
+    alert('O nome da tarefa não pode ser em branco para criar a tarefa!')
+  }
+}
+
+function teste(){
+  console.log('BTN clicado')
 }
